8946789ca71e2d81915a3b0e991a70fe18d2b7c0
hemant jayas <hemantjayas03#gmail.com>
2023-05-17 03:00:07 +0530

    port string wont

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = ()=>{
     console.log("database connected")
-    return mongoose.connect(`${process.env.DBURL}`)
+    return mongoose.connect("mongodb+srv://hemantjayas03:hem123@cluster0.xepnwbp.mongodb.net/users")
 }
 
 module.exports = connect;
